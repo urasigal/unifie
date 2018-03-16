5f41f809bfb26fd566463930b97381f498a61882
urasigal@gmail.com <urasigal@gmail.com>
2018-03-16 15:26:18 -0300

    Some progress.

--- tools/tools/src/main/java/zixi/yuv/tools/App.java
+++ tools/tools/src/main/java/zixi/yuv/tools/App.java
@@ -32,10 +32,28 @@ public class App
         			try (Stream<Path> paths = Files.walk(Paths.get(arrFoldersNames[i]))) {
         				ArrayList<String> folderFilesNames = new ArrayList<>();
         			    paths.filter(Files::isRegularFile)
-        			    	.forEach(fileName -> folderFilesNames.add(fileName.toString()));
+        			    	.forEach(fileName -> { 	
+        			    		String wholePathString = fileName.toString();
+        			    		String splittedPath[] = wholePathString.split("/");
+        			    		folderFilesNames.add(splittedPath[splittedPath.length-1]);
+        			    	});
         			    arrayOfArrayLists.add(folderFilesNames);
         			} 
 				}
+        		
+        		ArrayList<String> relativeArrayOfFileNames = arrayOfArrayLists.get(0);
+        		ArrayList<String> onlyFilesNames = null;
+        		for (int i = 0; i < relativeArrayOfFileNames.size(); i++) {
+        			String fileNameTocompare = relativeArrayOfFileNames.get(i);
+					for(int j = 0; j < arrayOfArrayLists.size(); j++)
+					{
+						if(arrayOfArrayLists.get(j).contains(fileNameTocompare))
+						{
+							onlyFilesNames.add(fileNameTocompare);
+						}
+					}
+				}
+        		System.out.println("Done");
         	}else throw new Exception("Please provide more than one folder pathes");
         	
         } catch (Exception e) {
